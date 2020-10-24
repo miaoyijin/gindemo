160da4caeb3e533db00b3ff4218a5d5853241efb
mouyj <mouyj@2345.com>
2020-10-24 20:02:18 +0800

    静态资源文件

--- routers/Router.go
+++ routers/Router.go
@@ -1,11 +1,18 @@
 package routers
 
-import "github.com/gin-gonic/gin"
+import (
+	"github.com/gin-gonic/gin"
+	"net/http"
+)
 
 func RegisterRoute(r *gin.Engine)  {
 	r.GET("/ping", func(c *gin.Context) {
 		c.JSON(200, gin.H{
 			"message": "pong",
 		})
+	})
+	r.LoadHTMLGlob("./public/html/*")
+	r.GET("/index.html", func(c *gin.Context) {
+		c.HTML(http.StatusOK, "index.html", nil)
 	})
 }
